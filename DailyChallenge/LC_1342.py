b12b4e15cb335f874e6cc61b677ad8b58070889e
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-03-01 19:12:40 -0500

    Create LC_1342.py

---
+++ DailyChallenge/LC_1342.py
@@ -0,0 +1,18 @@
+# 1342. Number of Steps to Reduce a Number to Zero
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        
+        step = 0
+        
+        while num > 0:
+            
+            if num % 2 == 0:
+                num = num // 2
+                step += 1
+            
+            else:
+                num -= 1
+                step += 1
+                
+        return step
+            
