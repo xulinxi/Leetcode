aa9e69d008cd5bc731c94ff84780a24621dc58eb
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-03-07 11:28:11 -0500

    Update LC_145.py with recursive method

--- DailyChallenge/LC_145.py
+++ DailyChallenge/LC_145.py
@@ -1,5 +1,3 @@
-# 145. Binary Tree Postorder Traversal
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,36 +7,36 @@
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         
-        res = []
-        stack_list = []
+#         res = []
+#         stack_list = []
 
         
-        while root or stack_list:
-            while root:
-                if root.right:
-                    stack_list.append(root.right)
-                stack_list.append(root)
-                # Keep looping until we go the leftmost leaf
-                root = root.left
+#         while root or stack_list:
+#             while root:
+#                 if root.right:
+#                     stack_list.append(root.right)
+#                 stack_list.append(root)
+#                 # Keep looping until we go the leftmost leaf
+#                 root = root.left
             
-            # Root should be pop to get its root.right value
-            root = stack_list.pop()
+#             # Root should be pop to get its root.right value
+#             root = stack_list.pop()
                 
-            # Check if right tree has been proceed
-            if stack_list and stack_list[-1] == root.right:
-                # put the root back to the list for later check
-                stack_list[-1] = root
-                # Set right node of the root as the root to check if there's any more children
-                root = root.right
+#             # Check if right tree has been proceed
+#             if stack_list and stack_list[-1] == root.right:
+#                 # put the root back to the list for later check
+#                 stack_list[-1] = root
+#                 # Set right node of the root as the root to check if there's any more children
+#                 root = root.right
                 
-            else:
-                # No more right node (and no more left node) -> get the value and reset node,
-                # so new root can be set by popping the next node that's available in the stack
-                res.append(root.val)
-                root = None
+#             else:
+#                 # No more right node (and no more left node) -> get the value and reset node,
+#                 # so new root can be set by popping the next node that's available in the stack
+#                 res.append(root.val)
+#                 root = None
 
     
-        return res
+#         return res
     
     
     
@@ -67,6 +65,26 @@ class Solution:
 #         return output
     
     
+    # -----------------------------------------------------------------
     
     # TODO: Recursive
         
+        res_list = []
+        
+        # Create a helper function to iterate via the tree, then call the helper function
+        def dfs(root):
+            if root:
+                
+                dfs(root.left)
+                dfs(root.right)
+                
+                # Just change the position when you append the root value
+                res_list.append(root.val)
+                
+        dfs(root)        
+                
+        return res_list
+                
+                
+            
+            
