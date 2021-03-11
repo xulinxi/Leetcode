e15811206714f0a0f440d61c2944ea152e123bb1
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-03-11 08:09:46 -0500

    Update LC_101.py

--- DailyChallenge/LC_101.py
+++ DailyChallenge/LC_101.py
@@ -8,32 +8,100 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        if root:
-            # if want to refer isMirror before implement it, we need to put self. before the method name
-            return self.isMirror(root.left, root.right)
-        else:
-            return True
+        
+        # Iterative Method--------------------------------------------------------
+        
+        
+        # (BFS)-------------------------------------------
+        queue = []
+        
+        queue.append(root)
+        queue.append(root)
+        
+        while queue:
+            leftSub = queue.pop(0)
+            rightSub = queue.pop(0)
             
-    def isMirror(self, left, right):
+            if leftSub is None and rightSub is None:
+                return True
+            if leftSub is None or rightSub is None:
+                return False
+            if leftSub.val != rightSub.val:
+                return False
+            
+            # If leftSub == RightSub:
+            queue.append(leftSub.left)
+            queue.append(rightSub.right)
+            queue.append(leftSub.right)
+            queue.append(rightSub.left)
+            
+        # If every goes through at the end -> finish checking every node -> mirror tree
+        return True
+            
+                
+                
+                
         
-        # List out two base cases: 1. empty 2. non-empty
         
-        # 1. empty: left and right both empty or just one side is empty
-        # Note: no need to compare their values, just to check if they exist.
-        if left is None and right is None:
-            return True
-        if left is None or right is None:
-            return False
         
-        # 2. non-empty
-        if left.val == right.val:
-            # check outer left and outer right, inner left and inner right
-            checkOuter = self.isMirror(left.left, right.right)
-            checkInner = self.isMirror(left.right, right.left)
-            return checkOuter and checkInner
-        else:
-            return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # Recursive Method--------------------------------------------------
+#         if root:
+#             # if want to refer isMirror before implement it, we need to put self. before the method name
+#             return self.isMirror(root.left, root.right)
+#         else:
+#             return True
+            
+#     def isMirror(self, left, right):
+        
+#         # List out two base cases: 1. empty 2. non-empty
+        
+#         # 1. empty: left and right both empty or just one side is empty
+#         # Note: no need to compare their values, just to check if they exist.
+#         if left is None and right is None:
+#             return True
+#         if left is None or right is None:
+#             return False
+        
+#         # 2. non-empty
+#         if left.val == right.val:
+#             # check outer left and outer right, inner left and inner right
+#             checkOuter = self.isMirror(left.left, right.right)
+#             checkInner = self.isMirror(left.right, right.left)
+#             return checkOuter and checkInner
+#         else:
+#             return False
 
+        # ----------------------------(referenced answer)
+#       def isSymmetric(self, root):
+#         if root is None:
+#           return True
+#         else:
+#           return self.isMirror(root.left, root.right)
+
+#       def isMirror(self, left, right):
+#         if left is None and right is None:
+#           return True
+#         if left is None or right is None:
+#           return False
+
+#         if left.val == right.val:
+#           outPair = self.isMirror(left.left, right.right)
+#           inPiar = self.isMirror(left.right, right.left)
+#           return outPair and inPiar
+#         else:
+#           return False
+        
+        
         # ----------------------------(referenced answer)
 #       def isSymmetric(self, root):
 #         if root is None:
