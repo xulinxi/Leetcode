2dbe9649ef9345c32aa29762674bd2beb8fbcd20
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-03-14 23:22:33 -0400

    Update LC_153.py

--- DailyChallenge/LC_153.py
+++ DailyChallenge/LC_153.py
@@ -4,6 +4,26 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         # When rotating, the direaction is going to the right like: --->; numbers moving to higher indices
         
+        # ----------------Divide and Conquer/Recursion----------------------------
+        lo = 0
+        hi = None
+        
+        def check_min(lo, hi):
+            
+            mid = (hi - lo) // 2 + lo
+            
+            # Base case: if only one number left -> it must be the smallest number
+            if lo == hi:
+                return nums[lo]
+            
+            # Remember the min in left and min in right
+            left_min = check_min(lo, mid)
+            right_min = check_min(mid + 1, hi)
+            
+            return left_min if left_min < right_min else right_min
+            
+
+        return check_min(lo, len(nums)-1)
         
         
         
