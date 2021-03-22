9ae6f66b43e9f7b48bcdeb5928a6703e8fcad8a9
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-03-22 07:08:13 -0400

    Update LC_53.py

--- DailyChallenge/LC_53.py
+++ DailyChallenge/LC_53.py
@@ -1,60 +1,54 @@
-# 53. Maximum Subarray
-
-
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-         # -------------------------#TODO: Divide and Conquer--------------------
+        
+        # --------------------- Divide and Conquer-------------------------
+        
+#         def cross_Sum(nums, low, high, mid):
+#             if low == high:
+#                 return nums[low] 
+            
+#             left_subsum = float('-inf')
+#             curr_sum = 0
+#             # (Starting with the middle then move toward the first number)
+#             # ***Note: low-1 because ending index is exclusive
+#             for i in range(mid, low - 1, -1):
+#                 curr_sum += nums[i]
+#                 left_subsum = max(curr_sum, left_subsum)
+            
+#             right_subsum = float('-inf')
+#             curr_sum = 0
+#             # (Starting with the middle then move back)
+#             for i in range(mid + 1, high + 1):
+#                 curr_sum += nums[i]
+#                 right_subsum = max(curr_sum, right_subsum)
+                
+#             return left_subsum + right_subsum
+            
+#         def helper_divCon(nums, low, high):
+#             if low == high:
+#                 return nums[low] 
+            
+#             mid = (high-low)//2 + low
+#             left_sum = helper_divCon(nums, low, mid) 
+#             right_sum = helper_divCon(nums, mid + 1, high) 
+#             cross_sum = cross_Sum(nums, low, high, mid)
+            
+#             return max(left_sum, right_sum, cross_sum)
+        
+        
+#         # *** return helper function to find the max sum
+#         return helper_divCon(nums, 0, len(nums) - 1)
         
         
         
-        
-        # -----------------_Sliding Window:------------
+        # --------------Greedy-----------------------------
         answer = nums[0]
         total = nums[0]
         
+        # Since we've stored index 0 value, we can just start from index 1
+        # Mistake: for i in range(1, len(nums)-1): -> len(nums)-1 cannot be reached
         for i in range(1, len(nums)):
-            # total is recording the maximun sum from past numbers
-            total = max(nums[i], nums[i]+total)
-            
+            total = max(total + nums[i], nums[i])
             answer = max(total, answer)
-            # if total > answer:
-            #     answer = total
-                
+            
         return answer
-        
-        
-        
-        
-        
-        
-        # -----------Brute Force----------------(Wrong answer)
-#         max_sum = float('-inf')
-#         lo, hi = 0, 0
-        
-#         # If only one number exists in the list
-#         if len(nums) == 1:
-#             return nums[0]
-        
-#         # Else: more than one item in the list
-#         for lo in range(len(nums)):
-#             temp_sum = 0
-#             temp_sum += nums[lo]
-#             # check if each number can has the largest sum
-#             max_sum = max(max_sum, temp_sum)
-            
-#             # add the hi (end of the window to the sum)
-#             for hi in range(len(nums)):
-#                 temp_sum += nums[hi]
-#                 max_sum = max(max_sum, temp_sum)
-                
-#         return max_sum
-                
-            
-            
-            
-            
-            
-            
-            
-            
-            
