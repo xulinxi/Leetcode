e368b63c6d2a1a6199c135bf6f57281f3da1a2f0
xulinxi <52603892+xulinxi@users.noreply.github.com>
2021-03-31 09:45:30 -0400

    Create LC_167.py

---
+++ DailyChallenge/LC_167.py
@@ -0,0 +1,21 @@
+# 167. Two Sum II - Input array is sorted
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+#         # *** (1-indexed) means returning the indices when index starting with 1 NOT 0!!!
+            
+#         i, j = 0, len(numbers) - 1
+        
+#         while i < j:
+#             if numbers[i] + numbers[j] == target:
+#                 return [i+1, j+1]
+            
+#             if numbers[i] + numbers[j] > target:
+#                 j -= 1
+                
+#             if numbers[i] + numbers[j] < target:
+#                 i += 1     
+                
+        
