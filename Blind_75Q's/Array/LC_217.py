d69f9dcec009ab3abadc7b19a51dfc46321e46cb
xulinxi <52603892+xulinxi@users.noreply.github.com>
2022-09-08 20:40:40 -0400

    Create LC_217.py

---
+++ Blind_75Q's/Array/LC_217.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for num in nums:
+            if num in visited:
+                return True
+            else:
+                visited.add(num)
+
+        return False
